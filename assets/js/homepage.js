f862e8f2a69c9b16a2676f4f69219d330b27a2d5
Britt Carlton <brittanyrc95@gmail.com>
2022-06-21 12:08:41 -0400

    Add ability to dynamically generate and display repo list items upon search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,11 @@
+// global variables
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 // functions
+// function to request a list of repos for a particular user from GitHub
 function getUserRepos(user) {
     // format the GitHub api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,9 +17,65 @@ function getUserRepos(user) {
             response.json()
                 .then(function(data) {
                     // display the formatted data of response
-                    console.log(data);
+                    displayRepos(data, user);
                 });
         });
 };
+// function to display the returned list of repos
+function displayRepos(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+    
+    // clear prior content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
 
-getUserRepos("devBritt");
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+        // create a span element to hold the repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        };
+
+        // append status to container
+        repoEl.appendChild(statusEl);
+
+        // append container to the DOM
+        repoContainerEl.appendChild(repoEl);
+    };
+};
+
+// event handlers
+function formSubmitHandler(event) {
+    event.preventDefault();
+    
+    // get value from input element\
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username.");
+    };
+};
+
+// event listeners
+userFormEl.addEventListener("submit", formSubmitHandler);

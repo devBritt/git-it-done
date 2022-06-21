f5a2fb0cdbdf487d65f11f084bd4b227952a1344
Britt Carlton <brittanyrc95@gmail.com>
2022-06-21 11:14:01 -0400

    Create function to request repos by user

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,18 @@
-// request list of repos by octocat from GitHub
-fetch("https://api.github.com/users/octocat/repos")
-    .then(function(response) {
-        // format the response
-        response.json()
-            .then(function(data) {
-                // display the formatted data of response
-                console.log(data);
-            });
-    });
+// functions
+function getUserRepos(user) {
+    // format the GitHub api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+            // format the response
+            response.json()
+                .then(function(data) {
+                    // display the formatted data of response
+                    console.log(data);
+                });
+        });
+};
+
+getUserRepos("facebook");
